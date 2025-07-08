16fe323f4b4e793d5fa2e36ab3f851cb844b8f84
Sanketmail3716 <malisanket2509@gmail.com>
2025-07-08 08:55:50 +0530

    Initial commit :GSM SMA and call Project using LPC2148

---
+++ Objects/gsm_full_final_code.d
@@ -0,0 +1,7 @@
+.\objects\gsm_full_final_code.o: GSM_Full_Final_code.c
+.\objects\gsm_full_final_code.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
+.\objects\gsm_full_final_code.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\gsm_full_final_code.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\gsm_full_final_code.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\gsm_full_final_code.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\gsm_full_final_code.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
